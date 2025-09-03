36def0e39e27faf98fd200afd5babd60ac3de4b0
Khang <khangphan1310@gmail.com>
2025-09-02 22:33:12 -0700

    initialize

---
+++ backend/resumeGenarator/src/main/java/com/example/resumeGenarator/ResumeGenaratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumeGenarator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeGenaratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeGenaratorApplication.class, args);
+	}
+
+}
---
+++ backend/resumeGenarator/src/test/java/com/example/resumeGenarator/ResumeGenaratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resumeGenarator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeGenaratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
